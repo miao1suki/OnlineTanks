accae0f65711158f6eb08c0359cd6c0830dd82dd
miao <2565553717@qq.com>
2026-04-04 03:04:52 +0800

    完善房间创建、连接、断开事件监听逻辑

--- OnlineTanks/Assets/Scripts/Core/GameManager.cs
+++ OnlineTanks/Assets/Scripts/Core/GameManager.cs
@@ -1,16 +1,39 @@
+using Mirror;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject[] DontDestroy;
+    public GameObject LobbyCanvas;
     public static GameManager instance;
     private void Awake()
     {
+        // 单例防重复
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         instance = this;
         DontDestroyOnLoad(gameObject);
     }
+    private void OnEnable()
+    {
+        NetworkManagerCustom.OnConnectionStatusChanged += HandleConnectionStatus;
+    }
+    private void OnDisable()
+    {
+        NetworkManagerCustom.OnConnectionStatusChanged -= HandleConnectionStatus;
+    }
     void Start()
     {
-        
+        foreach (GameObject obj in DontDestroy)
+        {
+            if (obj == null) continue;
+
+            DontDestroyOnLoad(obj);
+        }
     }
 
     void Update()
@@ -18,8 +41,46 @@ public class GameManager : MonoBehaviour
         
     }
 
+    private void HandleConnectionStatus(NetworkManagerCustom.ConnectionType type)
+    {
+        switch (type)
+        {
+            case NetworkManagerCustom.ConnectionType.ServerStart:
+                OnServerStarted();
+                break;
+            case NetworkManagerCustom.ConnectionType.ClientConnect:
+                OnClientConnected();
+                break;
+            case NetworkManagerCustom.ConnectionType.Disconnect:
+                OnDisconnected();
+                break;
+        }
+    }
+
+    private void OnServerStarted()
+    {
+        Debug.Log("服务端房间启动逻辑");
+        LobbyCanvas?.SetActive(false);
+        // 在这里初始化房间状态或玩家列表
+    }
+
+    private void OnClientConnected()
+    {
+        Debug.Log("客户端加入房间逻辑");
+        LobbyCanvas?.SetActive(false);
+    }
+
+    private void OnDisconnected()
+    {
+        Debug.Log("断开连接逻辑");
+        LobbyCanvas?.SetActive(true);
+    }
+
+
     public void ExitGame()
     {
         Debug.Log("退出游戏");
+        Application.Quit();
     }
+
 }
--- OnlineTanks/Assets/Scripts/Network/NetworkManagerCustom.cs
+++ OnlineTanks/Assets/Scripts/Network/NetworkManagerCustom.cs
@@ -3,19 +3,46 @@ using System;
 using Mirror;
 public class NetworkManagerCustom : NetworkManager
 {
-    public static Action<bool> OnConnectionStatusChanged;
+    // 连接类型枚举
+    public enum ConnectionType
+    {
+        ServerStart,   // 自己开房间的服务端
+        ClientConnect, // 客户端连接成功
+        Disconnect     // 断开连接
+    }
 
+    // 事件：参数标识连接类型
+    public static Action<ConnectionType> OnConnectionStatusChanged;
+
+    // 客户端成功连接
     public override void OnClientConnect()
     {
         base.OnClientConnect();
-        Debug.Log("连接成功");
-        OnConnectionStatusChanged?.Invoke(true);
+        Debug.Log("客户端连接成功");
+        OnConnectionStatusChanged?.Invoke(ConnectionType.ClientConnect);
     }
 
+    // 客户端断开连接
     public override void OnClientDisconnect()
     {
         base.OnClientDisconnect();
-        Debug.Log("连接断开");
-        OnConnectionStatusChanged?.Invoke(false);
+        Debug.Log("客户端断开连接");
+        OnConnectionStatusChanged?.Invoke(ConnectionType.Disconnect);
+    }
+
+    // 服务端启动（房间创建）
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        Debug.Log("服务端启动，房间已创建");
+        OnConnectionStatusChanged?.Invoke(ConnectionType.ServerStart);
+    }
+
+    // 服务端停止
+    public override void OnStopServer()
+    {
+        base.OnStopServer();
+        Debug.Log("服务端停止，房间已关闭");
+        OnConnectionStatusChanged?.Invoke(ConnectionType.Disconnect);
     }
 }
